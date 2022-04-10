030a56d732ef99e0c90c3ff6816987d096b00a73
cctorres <c.torresalzate@gmail.com>
2022-04-10 15:50:40 -0500

    server

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const { ApolloServer } = require("apollo-server");
-const connectDB = require("./db");
-const typeDefs = require("./graphql/types");
-const resolvers = require("./graphql");
-const models = require("./models");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./graphql/schema');
+const { connectDB } = require('./db/index');
+const { authenticated } = require('./middlewares/auth');
 
 connectDB();
+const app = express();
 
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers,
-  context: {models}
-});
+app.use(authenticated);
 
-server.listen({ port: 3000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+app.use('/', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(3000)
+console.log('Server is running on port 3000');
